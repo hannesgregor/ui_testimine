5bc65decf57c82f178cbd0734d00ddf2ee06cb78
hannesgregor <hannes-gregor.galkin@khk.ee>
2021-04-29 19:51:55 +0300

    Testing API

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://outside-in-dev-api.herokuapp.com/klKXqxazOcgMRM70idtnUbxKizU2rETg',
+});
+
+
+const api = {
+    loadRestaurants() {
+        return client.get('/restaurants').then(response => response.data);
+  },
+};
+  export default api;
---
+++ src/store/restaurants.js
@@ -0,0 +1,21 @@
+const restaurants = api => ({
+    namespaced: true,
+    state: {
+      records: [],
+    },
+    actions: {
+      load({commit}) {
+        api.loadRestaurants().then(records => {
+          commit('storeRecords', records);
+        });
+      },
+    },
+
+    mutations: {
+    storeRecords(state, records) {
+    state.records = records;
+    },
+  },
+});
+  
+  export default restaurants;
